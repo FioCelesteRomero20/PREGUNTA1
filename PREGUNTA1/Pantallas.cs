6f9c4c34ac3734eee1c2004c4c350da8fc5af8d9
PC <PC@DESKTOP-NOE8GP1>
2023-11-03 17:57:55 -0500

    Agregar archivos de proyecto.

---
+++ PREGUNTA1/Pantallas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PREGUNTA1
+{
+    public class Pantallas
+    {
+        public static void PieDePagina(float sumaPesos, int menosDe60Kg, int masDe100Kg, double promedio)
+        {
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Procesando...");
+            Console.WriteLine("=============================================");
+            Console.WriteLine("La suma de todos los pesos es: " + sumaPesos + " kg");
+            Console.WriteLine("El promedio de todos los pesos es: " + promedio.ToString("F1") + " kg");
+            Console.WriteLine("==============================================");
+            Console.WriteLine("Menos de 60 kg: " + menosDe60Kg);
+            Console.WriteLine("Mas de 100 kg: " + masDe100Kg);
+            Console.ReadKey();
+        }
+
+        public static int Cabecera()
+        {
+            Console.WriteLine("=================================");
+            int numero = Operaciones.getEntero("Ingrese el número de pesos a analizar: ");
+            return numero;
+        }
+    }
+}
---
+++ PREGUNTA1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PREGUNTA1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = Pantallas.Cabecera();
+
+            if (numero > 300)
+            {
+                Console.WriteLine("El número de pesos no puede ser mayor a 300.");
+                return;
+            }
+
+            float[] pesos = new float[numero];
+            float sumaPesos = 0;
+            int menosDe60Kg = 0;
+            int masDe100Kg = 0;
+
+            for (int i = 0; i < numero; i++)
+            {
+                pesos[i] = Operaciones.getDecimal("Ingrese el peso de la persona #" + (i + 1) + " (en kg): ");
+                sumaPesos += pesos[i];
+
+                if (pesos[i] < 60)
+                {
+                    menosDe60Kg++;
+                }
+                else if (pesos[i] > 100)
+                {
+                    masDe100Kg++;
+                }
+            }
+            float promedio = sumaPesos / numero;
+            Pantallas.PieDePagina(sumaPesos, menosDe60Kg, masDe100Kg, promedio);
+        }
+    }
+}
